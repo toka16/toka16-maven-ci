a99241f04de54aeb6d4b5df18dd0349318766ab9
toka16 <tabra12@freeuni.edu.ge>
2015-04-26 11:48:02 +0400

    add 'hello world'

---
+++ maven-travis-ci/src/main/java/com/mycompany/maven_travis_ci/MainClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.com.mycompany.maven_travis_ci;
+
+/**
+ *
+ * @author toka
+ */
+public class MainClass {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
