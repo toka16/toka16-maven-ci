1b2fb8ebc87777ce0134b83507567ef59999fba9
toka16 <tabra12@freeuni.edu.ge>
2015-04-26 12:18:21 +0400

    add green test

---
+++ maven-travis-ci/src/test/java/UnitTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.java;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author toka
+ */
+public class UnitTest {
+    
+    public UnitTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void greenTest(){
+        assertEquals(0, 0);
+    }
+}
